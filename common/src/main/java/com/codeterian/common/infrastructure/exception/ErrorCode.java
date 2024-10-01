9379018ce75906a52ac5df9854f8609a8905d55a
asd42270 <lyb991113@gmail.com>
2024-10-01 23:07:06 +0900

    [Feat]: 공통 응답 객체 생성

---
+++ common/src/main/java/com/codeterian/common/infrastructure/exception/ErrorCode.java
@@ -0,0 +1,20 @@
+package com.codeterian.common.infrastructure.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public enum ErrorCode {
+    //User
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."),
+    ;
+
+    private final HttpStatus httpStatus;
+
+    private final String message;
+
+    ErrorCode(HttpStatus httpStatus, String message) {
+        this.httpStatus = httpStatus;
+        this.message = message;
+    }
+}
