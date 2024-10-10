d26ce03282006e00fc994c62e5c6762df08146c4
rkoji <wl0wn1212@gmail.com>
2024-10-10 22:26:10 +0900

    [Feat] Performance 등록, 수정 ResponseDto 추가

---
+++ performance/src/main/java/com/codeterian/performance/presentation/dto/response/PerformanceModifyResponseDto.java
@@ -0,0 +1,44 @@
+package com.codeterian.performance.presentation.dto.response;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import com.codeterian.performance.domain.performance.Performance;
+
+import lombok.Builder;
+
+@Builder
+public record PerformanceModifyResponseDto(
+	UUID id,
+	String title,
+	String description,
+	String location,
+	LocalDateTime startDate,
+	LocalDateTime endDate,
+	LocalDateTime bookingStartTime,
+	LocalDateTime bookingEndTime,
+	Integer duration,
+	String ageRestriction,
+	String status,
+	Integer ticketStock,
+	UUID categoryId
+) {
+
+	public static PerformanceModifyResponseDto fromEntity(Performance performance) {
+		return PerformanceModifyResponseDto.builder()
+			.id(performance.getId())
+			.title(performance.getTitle())
+			.description(performance.getDescription())
+			.location(performance.getLocation())
+			.startDate(performance.getStartDate())
+			.endDate(performance.getEndDate())
+			.bookingStartTime(performance.getBookingStartDate())
+			.bookingEndTime(performance.getBookingEndDate())
+			.duration(performance.getDuration())
+			.ageRestriction(performance.getAgeRestriction())
+			.status(performance.getStatus().name())
+			.ticketStock(performance.getTicketStock())
+			.categoryId(performance.getCategory().getId())
+			.build();
+	}
+}
