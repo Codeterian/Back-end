30f43f3a3e8bc93b4652e89025313440a42b66ef
asd42270 <lyb991113@gmail.com>
2024-10-14 23:49:55 +0900

    [Refactor]: 로그인 응답 양식 변경, 이메일 정규식 변경

--- common/src/main/java/com/codeterian/common/infrastructure/dto/UserAddRequestDto.java
+++ common/src/main/java/com/codeterian/common/infrastructure/dto/UserAddRequestDto.java
@@ -2,6 +2,7 @@ package com.codeterian.common.infrastructure.dto;
 
 import com.codeterian.common.infrastructure.entity.UserRole;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
@@ -12,10 +13,10 @@ public record UserAddRequestDto(
         @Size(min = 4, max = 12, message = "비밀번호는 4자 이상 12자 이하여야합니다.")
         String password,
         @NotBlank(message = "email은 필수값입니다.")
-        @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\\\.[a-zA-Z]+$", message = "이메일 형식에 맞게 입력해주세요")
+        @Pattern(regexp = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@[A-Za-z0-9][A-Za-z0-9-]*(\\.[A-Za-z]{2,})+$", message = "이메일 형식에 맞게 입력해주세요")
         String email,
 
-        @NotBlank(message = "userRole은 필수값입니다.")
+        @NotNull(message = "userRole은 필수값입니다.")
         UserRole userRole
 ) {
 
